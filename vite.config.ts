531160fdb2f7146feb2293ea8c9b7c9661d92462
Dmitry Romashov <framebassman@gmail.com>
2025-03-30 00:14:49 +0100

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), cloudflare(), sentryVitePlugin({
+    org: "kolenka-inc",
+    project: "inventory"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
