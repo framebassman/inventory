984bf7b4c44f347ab8b3769a6b2bd0d02d509662
Dmitry Romashov <framebassman@gmail.com>
2025-05-03 23:43:56 +0200

    Fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,25 +1,25 @@
-import { Hono } from "hono";
-import { withSentry } from "@sentry/cloudflare";
-import { log } from "./logger";
+import { Hono } from 'hono';
+import { withSentry } from '@sentry/cloudflare';
+import { log } from './logger';
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", async (c) => {
-  log.info("Hello world from Cloudflare and ElasticSearch");
-  console.log("Hello world from Cloudflare");
-  return c.json("ok");
+app.get('/api/', async (c) => {
+  log.info('Hello world from Cloudflare and ElasticSearch');
+  console.log('Hello world from Cloudflare');
+  return c.json('ok');
 });
 
-app.get("*", (c: any) => {
+app.get('*', (c: any) => {
   return c.env.ASSETS.fetch(c.req.raw);
 });
 
 export default withSentry(
   //@ts-expect-error it should be here
   (env: any) => {
-    return ({
-      dsn: "https://9b631feb01da428b471ac1cbd160173f@o164625.ingest.us.sentry.io/4509063427653632",
-    });
+    return {
+      dsn: 'https://9b631feb01da428b471ac1cbd160173f@o164625.ingest.us.sentry.io/4509063427653632'
+    };
   },
   app
 );
