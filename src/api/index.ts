98a6ed37aafb73f8df1fe3b561fe2b3a7681e048
Dmitry Romashov <framebassman@gmail.com>
2025-05-04 09:12:59 +0200

    Add routers

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,97 +1,29 @@
-import { type Context, Hono } from 'hono';
+import { type Context, type Env, Hono } from 'hono';
 import { withSentry } from '@sentry/cloudflare';
-import { drizzle } from 'drizzle-orm/d1';
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
-import { sql } from 'drizzle-orm';
 import { log } from './logger';
 import { logger as loggerMiddleware } from 'hono/logger';
-import postgres from 'postgres';
+import d1 from './routers/d1';
+import pg from './routers/pg';
 import 'reflect-metadata';
-
-const users = sqliteTable('users', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  name: text('name').notNull()
-});
 
 const app = new Hono<{ Bindings: Env }>();
 app.use(loggerMiddleware());
 
-app.get('/api/', async (c: any) => {
+app.get('/api/', async (c: Context) => {
   log.info('Hello world from Cloudflare and ElasticSearch');
-  const db = drizzle(c.env.DB);
   return c.json('ok');
 });
 
-app.get('/setup', async (context: Context) => {
-  log.info('Going to setup database');
-  const db = drizzle(context.env.DB);
-  await db.run(sql`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL
-    )
-  `);
-  return new Response('Table created or already exists!');
-});
+app.route('/d1', d1);
+app.route('/pg', pg);
 
-app.get('/add', async (context: Context) => {
-  log.info('Going to add a new user into database');
-  const db = drizzle(context.env.DB);
-  const newUser = await db
-    .insert(users)
-    .values({ name: 'Test User' })
-    .returning()
-    .get();
-
-  return Response.json(newUser);
-});
-
-app.get('/users', async (context: Context) => {
-  log.info('Going to get all users from database');
-  const db = drizzle(context.env.DB);
-  const allUsers = await db.select().from(users).all();
-  return Response.json(allUsers);
-});
-
-app.get('/pg', async (context: Context) => {
-  log.info('Going to connect to postgres');
-  // Create a connection using the Postgres.js driver (or any supported driver, ORM or query builder)
-  // with the Hyperdrive credentials. These credentials are only accessible from your Worker.
-  const sql = postgres(context.env.HYPERDRIVE.connectionString, {
-    // Workers limit the number of concurrent external connections, so be sure to limit
-    // the size of the local connection pool that postgres.js may establish.
-    max: 5,
-
-    // If you are not using array types in your Postgres schema,
-    // disabling this will save you an extra round-trip every time you connect.
-    fetch_types: false
-  });
-
-  try {
-    // Sample query
-    const results = await sql`SELECT * FROM pg_tables`;
-
-    // Clean up the client after the response is returned, before the Worker is killed
-    context.executionCtx.waitUntil(sql.end());
-
-    // Return result rows as JSON
-    return Response.json(results);
-  } catch (e) {
-    console.error(e);
-    return Response.json(
-      { error: e instanceof Error ? e.message : e },
-      { status: 500 }
-    );
-  }
-});
-
-app.get('*', (c: any) => {
+app.get('*', (c: Context) => {
   return c.env.ASSETS.fetch(c.req.raw);
 });
 
 export default withSentry(
   //@ts-expect-error it should be here
-  (env: any) => {
+  (env: Env) => {
     return {
       dsn: 'https://9b631feb01da428b471ac1cbd160173f@o164625.ingest.us.sentry.io/4509063427653632'
     };
---
+++ src/api/routers/pg.ts
@@ -0,0 +1,39 @@
+import { type Context, type Env, Hono } from 'hono';
+import postgres from 'postgres';
+import { log } from '../logger';
+
+const app = new Hono();
+
+app.get('/', async (context: Context) => {
+  log.info('Going to connect to postgres');
+  // Create a connection using the Postgres.js driver (or any supported driver, ORM or query builder)
+  // with the Hyperdrive credentials. These credentials are only accessible from your Worker.
+  const sql = postgres(context.env.HYPERDRIVE.connectionString, {
+    // Workers limit the number of concurrent external connections, so be sure to limit
+    // the size of the local connection pool that postgres.js may establish.
+    max: 5,
+
+    // If you are not using array types in your Postgres schema,
+    // disabling this will save you an extra round-trip every time you connect.
+    fetch_types: false
+  });
+
+  try {
+    // Sample query
+    const results = await sql`SELECT * FROM pg_tables`;
+
+    // Clean up the client after the response is returned, before the Worker is killed
+    context.executionCtx.waitUntil(sql.end());
+
+    // Return result rows as JSON
+    return Response.json(results);
+  } catch (e) {
+    console.error(e);
+    return Response.json(
+      { error: e instanceof Error ? e.message : e },
+      { status: 500 }
+    );
+  }
+});
+
+export default app;
