51898bec3cb2429677573480aa53750884969c2e
Dmitry Romashov <framebassman@gmail.com>
2025-05-09 13:48:44 +0200

    Revert "Feature/drizzle (#4)" (#5)
    This reverts commit a088a79ad766d657db23733ccd9e72dd3e34f770.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,35 +1,21 @@
-import { type Context, type Env, Hono } from 'hono';
+import { Hono } from 'hono';
 import { withSentry } from '@sentry/cloudflare';
 import { log } from './logger';
-import { logger as loggerMiddleware } from 'hono/logger';
-import d1 from './routers/d1';
-import pg from './routers/pg';
-import 'reflect-metadata';
 
 const app = new Hono<{ Bindings: Env }>();
-app.use(loggerMiddleware());
 
-app.get('/api/', async (c: Context) => {
+app.get('/api/', async (c) => {
   log.info('Hello world from Cloudflare and ElasticSearch');
   return c.json('ok');
 });
 
-app.route('/d1', d1);
-app.route('/pg', pg);
-
-app.get('/json', async () => {
-  return fetch(
-    'https://microsoftedge.github.io/Demos/json-dummy-data/64KB.json'
-  );
-});
-
-app.get('*', (c: Context) => {
+app.get('*', (c: any) => {
   return c.env.ASSETS.fetch(c.req.raw);
 });
 
 export default withSentry(
   //@ts-expect-error it should be here
-  (env: Env) => {
+  (env: any) => {
     return {
       dsn: 'https://9b631feb01da428b471ac1cbd160173f@o164625.ingest.us.sentry.io/4509063427653632'
     };
