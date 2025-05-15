50df7aa2618769d056eb4855634824a78c5ecd5b
Dmitry Romashov <framebassman@gmail.com>
2025-05-15 09:45:55 +0200

    Add a local postgres

---
+++ postgres/user.sql
@@ -0,0 +1,18 @@
+DO
+$do$
+BEGIN
+  IF NOT EXISTS (
+      SELECT
+  FROM pg_catalog.pg_roles
+  WHERE  rolname = 'inventory-user') THEN
+
+  CREATE ROLE store_user
+  LOGIN PASSWORD 'KqCQzyH2akGB9gQ4';
+  GRANT SELECT ON ALL TABLES IN SCHEMA public TO store_user;
+  ALTER ROLE store_user WITH CREATEDB;
+END
+IF;
+END
+$do$;
+
+CREATE EXTENSION pgcrypto;
