b0f72e8785139abe1fbc0539f7f97c9d0870f7dd
Dmitry Romashov <framebassman@gmail.com>
2025-05-25 09:52:47 +0200

    Add logs

--- api/src/services/warehouse-service.ts
+++ api/src/services/warehouse-service.ts
@@ -15,6 +15,7 @@ export class WarehouseService {
     // );
     // const now = new Date(nowIsoString);
     // 25.05.2025
+    console.log(new Date().toISOString());
     const name = new Date().toLocaleDateString('en-US', {
       year: 'numeric',
       month: '2-digit',
