4db827e9a5d0d3fb4b1b613c4130723b779bb77e
Dmitry Romashov <framebassman@gmail.com>
2025-05-25 17:08:41 +0200

    Add a temp version

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,3 +1,22 @@
 export class VerificationMethods {
   static Barcode = 'Barcode';
 }
+
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { thunk } from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { reducer as turnstileReducer } from './reducer';
+
+const reducers = {
+  turnstile: turnstileReducer
+};
+const rootReducer = combineReducers({
+  ...reducers
+});
+
+const middlewareEnhancer = applyMiddleware(thunk);
+
+// const composedEnhancers = composeWithDevTools(middlewareEnhancer);
+
+export const store = createStore(rootReducer, middlewareEnhancer);
