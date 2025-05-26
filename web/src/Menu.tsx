6e9139c3899ab258c1339cdd7443a9bfdebd2a4b
Dmitry Romashov <framebassman@gmail.com>
2025-05-26 12:22:26 +0200

    Navigation

--- web/src/Menu.tsx
+++ web/src/Menu.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 import RestoreIcon from '@mui/icons-material/Restore';
 import AddIcon from '@mui/icons-material/Add';
 import './Menu.css';
 
 export const Menu = () => {
-  const [btnValue, setButtonValue] = useState(0);
   const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const paths = ['/', '/assign'];
+  const [btnValue, setButtonValue] = useState(paths.findIndex(current => current === pathname));
   return (
     <>
       <BottomNavigation
