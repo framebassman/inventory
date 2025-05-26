9dd911b86721b7a60b6e5dea055886f959129f8d
Dmitry Romashov <framebassman@gmail.com>
2025-05-26 13:15:36 +0200

    Add codecov plugin

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig, coverageConfigDefaults } from 'vitest/config';
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 export default defineConfig({
+  plugins: [
+    // Put the Codecov vite plugin after all other plugins
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "inventory",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
   test: {
     reporters: [
       'default',
