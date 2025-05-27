18afe6a80cb3c20655f973f8907a28dc03f952ce
Dmitry Romashov <framebassman@gmail.com>
2025-05-27 19:22:17 +0200

    Fix UI add button

--- web/src/default-camera-app/warehouse/index.tsx
+++ web/src/default-camera-app/warehouse/index.tsx
@@ -1,36 +1,96 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router';
+import { useSearchParams } from 'react-router';
+import { extractExistingParams } from '../search-params-parser';
+
+import { Box, Button, CircularProgress, Typography } from '@mui/material';
+
 import './index.css';
 
+const addItemToWarehouseAsync = async (name: string, code: string): Promise<boolean> => {
+  await fetch(
+    'https://api.inventory.romashov.tech/warehouse/assign',
+    {
+      method: 'POST',
+      mode: "no-cors", // no-cors, *cors, same-origin
+      body: JSON.stringify({name, code})
+    }
+  );
+  return true;
+}
+
+interface ItemInfo {
+  code: string,
+  name: string,
+  exist: boolean,
+}
+
+const fetchItemInfoAsync = async (code: string): Promise<ItemInfo> => {
+  // `https://api.inventory.romashov.tech/warehouse/item/${code}`,
+  const resp = await fetch(`https://petstore.swagger.io/v2/pet/${code}`);
+  try {
+    const json = await resp.json();
+    return {
+      code: code,
+      name: json.name,
+      exist: true,
+    } as ItemInfo;
+  } catch {
+    return {
+      code: code,
+      name: '',
+      exist: false,
+    } as ItemInfo;
+  }
+}
+
 export const Warehouse = () => {
-  const { search } = useLocation();
-  const [info, setInfo] = useState<string>("");
-  useEffect(() => {
-    let ignore = false;
+  const [search] = useSearchParams();
+  const [info, setInfo] = useState<ItemInfo | null>(null);
+  const [disabled, setDisabled] = useState(false);
+  const params = extractExistingParams(search);
+  const code = params.item[0];
+  useEffect(
+    () => {
+      let ignore = false;
 
-    async function startFetchingAsync() {
-      const response = await fetch('https://petstore.swagger.io/v2/pet/1');
-      if (!ignore) {
-        try {
-          const message = await response.json();
-          console.log(JSON.stringify(message));
-          setInfo(JSON.stringify(message));
-        } catch (error) {
-          console.error(error);
-        }
+      async function startFetchingAsync() {
+        const info = await fetchItemInfoAsync(code);
+        setInfo(info);
       }
-    }
 
-    if (search != '') {
-      startFetchingAsync();
-    }
+      if (code != '') {
+        startFetchingAsync();
+      }
 
-    return () => {
-      ignore = true;
-    };
-  }, [search]);
+      return () => {
+        ignore = true;
+      };
+    },
+    []
+  );
   
   return (
-    <div>{search}</div>
+    <Box>
+      { info === null
+        ? <CircularProgress color='secondary' />
+        : <>
+            <Box className="element">
+              <Typography textAlign="center">{info?.name}</Typography>
+            </Box>
+            <Box className="element">
+              <Button
+                variant='contained'
+                onClick={async () => {
+                  setDisabled(true);
+                  await addItemToWarehouseAsync(String(info?.name), code);
+                }}
+                disabled={disabled}
+              >
+                Добавить
+              </Button>
+            </Box>
+          </>
+      }
+    </Box>
   )
 }
