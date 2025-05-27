8fe68ce7ebfff420d525fb74b121ee5f72804202
Dmitry Romashov <framebassman@gmail.com>
2025-05-27 15:32:48 +0200

    Pass the ngrok config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,12 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ngrok } from 'vite-plugin-ngrok'
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import YAML from 'yaml'
+
+const ngrokConfig = YAML.parse(fs.readFileSync(path.resolve('/home/d.romashov/snap/ngrok/265/.config/ngrok/ngrok.yml'), 'utf-8'));
 
 export default defineConfig({
   plugins: [
@@ -9,13 +15,13 @@ export default defineConfig({
       org: 'kolenka-inc',
       project: 'inventory',
       authToken: process.env.SENTRY_AUTH_TOKEN
-    })
+    }),
+    ngrok({
+      authtoken: ngrokConfig.authtoken,
+    }),
   ],
 
   build: {
     sourcemap: true
   },
-  // server: {
-  //   allowedHosts: [ process.env.NGROK_HOST.toString() ]
-  // }
 });
