4fa29a2a1b91048f107054ce941401f51d05f6a6
Dmitry Romashov <framebassman@gmail.com>
2025-05-28 19:25:20 +0200

    Invoke different methods depends on the swithc

--- web/src/default-camera-app/movement/index.tsx
+++ web/src/default-camera-app/movement/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
+import { useLocalStorage } from '@uidotdev/usehooks';
+import type { ItemInfo } from '../item-info';
 import CheckIcon from '@mui/icons-material/Check';
-
 import {
   Box,
   CircularProgress,
@@ -10,9 +11,10 @@ import {
 } from '@mui/material';
 
 import './index.css';
-import type { ItemInfo } from '../item-info';
+import { SessionState } from '../model';
 
-const addItemToMovementAsync = async (code: string): Promise<ItemInfo> => {
+
+const addItemToDeparturesAsync = async (code: string): Promise<ItemInfo> => {
   try {
     const resp = await fetch(
       `https://api.inventory.romashov.tech/movement/item`,
@@ -36,11 +38,39 @@ const addItemToMovementAsync = async (code: string): Promise<ItemInfo> => {
   }
 };
 
+const addItemToArrivalsAsync = async (code: string): Promise<ItemInfo> => {
+  try {
+    const resp = await fetch(
+      `https://api.inventory.romashov.tech/movement/item`,
+      {
+        method: "DELETE",
+        body: JSON.stringify({ code: code })
+      }
+    );
+    const json = await resp.json();
+    return {
+      code: code,
+      name: json.name,
+      exist: resp.ok
+    } as ItemInfo;
+  } catch {
+    return {
+      code: code,
+      name: '',
+      exist: false
+    } as ItemInfo;
+  }
+};
+
 export const Movement = () => {
   const [search] = useSearchParams();
   const [info, setInfo] = useState<ItemInfo | null>(null);
   const [submitting, setSubmitting] = useState<boolean>(false);
   const code = String(search.get('item'));
+  const [sessionState] = useLocalStorage(
+    'SessionState',
+    SessionState.Departure
+  );
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -49,9 +79,15 @@ export const Movement = () => {
 
     async function startFetchingAsync() {
       setSubmitting(true);
-      const info = await addItemToMovementAsync(code);
-      setInfo(info);
-      setSubmitting(false);
+      if (sessionState == SessionState.Arrival) {
+        const info = await addItemToArrivalsAsync(code);
+        setInfo(info);
+        setSubmitting(false);
+      } else {
+        const info = await addItemToDeparturesAsync(code);
+        setInfo(info);
+        setSubmitting(false);
+      }
     }
 
     if (code != '') {
@@ -75,7 +111,10 @@ export const Movement = () => {
     return (
       <Box>
         <Box className="element">
-          <Typography>Добавляем...</Typography>
+          { sessionState === SessionState.Departure
+            ? <Typography>Берём на саунд-чек...</Typography>
+            : <Typography>Берём на базу...</Typography>
+          }
         </Box>
         <Box className="element">
           <CircularProgress/>
@@ -88,7 +127,10 @@ export const Movement = () => {
     <Box>
       <Box className="element">
         <Typography>
-          <span>Добавили </span>
+          { sessionState === SessionState.Departure
+            ? <span>Взяли на саунд-чек </span>
+            : <span>Взяли на базу </span>
+          }
           <span>{info?.name}</span>
         </Typography>
       </Box>
