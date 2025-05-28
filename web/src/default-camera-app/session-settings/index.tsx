a5da382bd3e8bca1357f6a7b009720b0597fcefb
Dmitry Romashov <framebassman@gmail.com>
2025-05-28 11:40:46 +0200

    Add the session settings

--- web/src/default-camera-app/session-settings/index.tsx
+++ web/src/default-camera-app/session-settings/index.tsx
@@ -1,7 +1,86 @@
-import "./index.css";
+import { useEffect, useState, useActionState } from 'react';
+
+import {
+  Box,
+  Button,
+  CircularProgress,
+} from '@mui/material';
+
+import './index.css';
+
+const fetchSessionInfoAsync = async (): Promise<boolean> => {
+  const resp = await fetch(
+    `https://api.inventory.romashov.tech/movement/current`
+  );
+  try {
+    const json = await resp.json();
+    return json.hasBeenStarted;
+  } catch {
+    return false;
+  }
+};
+
+const bakeBunAsync = async () => {
+  const resp = await fetch(
+    `https://api.inventory.romashov.tech/movement/current`,
+    { method: "POST" }
+  );
+  try {
+    const json = await resp.json();
+    return json.hasBeenStarted;
+  } catch {
+    return false;
+  }
+};
 
 export const SessionSettings = () => {
+  const [hasBeenStarted, setHasBeenStarted] = useState<boolean | null>(null);
+  const [_, bakeBunAction, isBaking] = useActionState(bakeBunAsync, false);
+  
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    let ignore = false;
+
+    async function startFetchingAsync() {
+      const info = await fetchSessionInfoAsync();
+      setHasBeenStarted(info);
+    }
+
+    if (!ignore) {
+      startFetchingAsync();
+    }
+
+    return () => {
+      ignore = true;
+    };
+  }, [hasBeenStarted]);
+  
+  if (hasBeenStarted === null) {
+    return (
+      <Box>
+        <CircularProgress color="secondary" />
+      </Box>
+    )
+  }
+
   return (
-    <div>Hi from SessionSettings</div>
+    <Box>
+      <div>
+      <form
+        action={bakeBunAction}
+        method='POST'
+      >
+        <Button
+          variant="contained"
+          color="secondary"
+          type="submit"
+          disabled={isBaking || Boolean(hasBeenStarted)}
+        >
+          {isBaking || hasBeenStarted ? 'Уже собираемся...' : 'Начать собираться'}
+        </Button>
+      </form>
+      </div>
+    </Box>
   )
 }
