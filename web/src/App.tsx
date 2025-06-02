b65f470a65289f7dd1361a96b41d5a3f3ac0e723
Dmitry Romashov <framebassman@gmail.com>
2025-06-03 00:03:54 +0200

    Add the landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import inventoryLogo from "./assets/inventory-circles-in-square.png";
+import iosStep1 from "./assets/ios_1.png";
+import iosStep2 from "./assets/ios_2.png";
+import iosStep3 from "./assets/ios_3.png";
+import androidStep1 from "./assets/android_1.png";
+import androidStep2 from "./assets/android_2.png";
+import androidStep3 from "./assets/android_3.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://inventory.romashov.tech" target="_blank">
+          <img src={inventoryLogo} className="logo" alt="Inventory logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Инвéнтори — решения для складских помещений</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        С помощью гугл таблиц поможем отследить, куда какой ящик уехал.
+      </div>
+      <h2>Инструкция для iOS</h2>
+      <div className="slider">
+        <div className="step">
+          <img src={iosStep1} className="step-icon" alt="Inventory logo" />
+          <div>
+            <h3>Шаг 1</h3>
+            <p>
+              Зайдите в Safari на avito.ru и нажмите на значок «Поделиться»
+              внизу экрана.
+            </p>
+          </div>
+        </div>
+
+        <div className="step">
+          <img src={iosStep2} className="step-icon" alt="Inventory logo" />
+          <div>
+            <h3>Шаг 2</h3>
+            <p>
+              Во всплывающем окне открутите вниз и выберите «На экран „Домой“».
+            </p>
+          </div>
+        </div>
+
+        <div className="step">
+          <img src={iosStep3} className="step-icon" alt="Inventory logo" />
+          <div>
+            <h3>Шаг 3</h3>
+            <p>
+              Нажмите «Добавить» — значок сайта появится рядом с другими
+              приложениями.
+            </p>
+          </div>
+        </div>
+      </div>
+      <h2>Инструкция для Android</h2>
+      <div className="slider">
+        <div className="step">
+          <img src={androidStep1} className="step-icon" alt="Inventory logo" />
+          <div>
+            <h3>Шаг 1</h3>
+            <p>
+              Наведите камеру телефона на QR-код или скачайте файл по ссылке.
+            </p>
+            <p>
+              Если увидите уведомление безопасности, нажмите «Всё равно скачать»
+              — приложение надежное и безопасное.
+            </p>
+          </div>
+        </div>
+
+        <div className="step">
+          <img src={androidStep2} className="step-icon" alt="Inventory logo" />
+          <div>
+            <h3>Шаг 2</h3>
+            <p>
+              После скачивания найдите файл в загрузках и начните установку.
+            </p>
+          </div>
+        </div>
+
+        <div className="step">
+          <img src={androidStep3} className="step-icon" alt="Inventory logo" />
+          <div>
+            <h3>Шаг 3</h3>
+            <p>После установки приложение появится на экране смартфона.</p>
+          </div>
+        </div>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Инвентори — часть продуктов компании Kolenka Inc.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
